da8dc5d934afead240a05419063d531588446b72
Toan Le <toan.lee@outlook.com>
2023-03-30 21:42:21 +0200

    testing locally

--- airflowv2-kubernetes/configure-kind-cluster.sh
+++ airflowv2-kubernetes/configure-kind-cluster.sh
@@ -12,15 +12,15 @@ main() {
   echo "You are going to init and run airflow version:$airflow_version on kubernetes with kind"
   ###
 
-  echo "Verifying google-cloud-cli config on local..."
-  local gcloud_path airflow_gcloud_path
-    gcloud_path=$(gcloud info --format=json | grep global_config_dir | cut -d ':' -f 2 | tr -d '," ' | sed 's/\//\\&/g')   
-    airflow_gcloud_path=$(echo /home/airflow/.config/gcloud | sed 's/\//\\&/g')
+  # echo "Verifying google-cloud-cli config on local..."
+  # local gcloud_path airflow_gcloud_path
+  #   gcloud_path=$(gcloud info --format=json | grep global_config_dir | cut -d ':' -f 2 | tr -d '," ' | sed 's/\//\\&/g')   
+  #   airflow_gcloud_path=$(echo /home/airflow/.config/gcloud | sed 's/\//\\&/g')
 
-  if [[ -z $gcloud_path ]]; then
-    echo "> Error reading gcloud config"
-    exit 1
-  fi
+  # if [[ -z $gcloud_path ]]; then
+  #   echo "> Error reading gcloud config"
+  #   exit 1
+  # fi
 
   echo "Configuring kind-cluster.yaml file..."
 
@@ -28,7 +28,7 @@ main() {
   cp kind-cluster.temp.yaml kind-cluster.yaml
 
   # Add volume mount for gcloud
-  sed -i.bak -E "/^  extraMounts:/s//  extraMounts:\n    - hostPath: $gcloud_path\n      containerPath: $airflow_gcloud_path/" kind-cluster.yaml  
+  # sed -i.bak -E "/^  extraMounts:/s//  extraMounts:\n    - hostPath: $gcloud_path\n      containerPath: $airflow_gcloud_path/" kind-cluster.yaml  
 }
 
 # Run the main funtion
